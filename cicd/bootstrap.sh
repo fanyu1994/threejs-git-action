5888c57e4a43b6c2f4096900239a5ac14bb49530
fanyu <1025153697@qq.com>
2023-02-20 15:31:51 +0800

    merge main

---
+++ cicd/bootstrap.sh
@@ -0,0 +1,18 @@
+echo  -e "------------登陆镜像服务器---------------"
+docker login --username=$1 registry.cn-hangzhou.aliyuncs.com --password=$2
+
+echo -e "-------------停止容器------------------"
+docker stop fy-three
+
+echo -e "-------------删除容器----------------"
+docker rm fy-three
+
+echo -e "-------------删除镜像------------------"
+docker rmi registry.cn-hangzhou.aliyuncs.com/fanyu1994/fy-three:latest
+
+echo -e "-------------拉新的镜像--------------"
+docker pull registry.cn-hangzhou.aliyuncs.com/fanyu1994/fy-three:latest
+
+echo -e "-----------------构建镜像-----------------"
+docker run --rm -d --name fy-three -p 8787:80 registry.cn-hangzhou.aliyuncs.com/fanyu1994/fy-three:latest
+echo -e "-------------------构建镜像完毕-----------------"
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<script setup lang="ts">
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { onMounted, defineProps } from 'vue'
+
+interface Props {}
+
+defineProps<Props>()
+
+const width = window.innerWidth
+const height = 800
+onMounted(() => {
+  // 创建场景、相机和渲染器
+  var scene = new THREE.Scene()
+  var camera = new THREE.PerspectiveCamera(
+    75,
+    width/height,
+    0.1,
+    1000
+  )
+  var renderer = new THREE.WebGLRenderer()
+
+  renderer.setSize(width, height)
+  document.querySelector('.card')!.appendChild(renderer.domElement)
+
+  // 创建一个球体，并将全景图片作为纹理贴上去
+  var geometry = new THREE.SphereGeometry(500, 60, 40)
+  geometry.scale(-1, 1, 1)
+  var texture = new THREE.TextureLoader().load('src/assets/bg2.jpg')
+  var material = new THREE.MeshBasicMaterial({ map: texture })
+  var sphere = new THREE.Mesh(geometry, material)
+  scene.add(sphere)
+
+  // 添加OrbitControls控制器
+  var controls = new OrbitControls(camera, renderer.domElement)
+  controls.autoRotate = true // 设置控制器自动旋转
+  controls.enableDamping = true // 启用阻尼效果
+  controls.dampingFactor = 0.1 // 阻尼系数，越小阻尼越强
+  controls.rotateSpeed = 0.5 // 旋转速度
+
+  // 设置相机位置
+  camera.position.set(0, 0, 1)
+  camera.lookAt(sphere.position)
+
+  function animate() {
+    requestAnimationFrame(animate)
+    controls.update() // 更新控制器
+    renderer.render(scene, camera)
+  }
+  animate()
+})
+</script>
+
+<style>
+.card {
+  width: 100%;
+  height: 400px;
+}
+</style>
